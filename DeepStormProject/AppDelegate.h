0ae22d9d1bb31c9230cf8df447e041ee5d54ca7c
Alexandr Babenko <Hikto583004@list.ru>
2016-08-07 16:46:32 +0500

    Initial Commit

---
+++ DeepStormProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeepStormProject
+//
+//  Created by Alexandr Babenko on 07.08.16.
+//  Copyright © 2016 Alexandr Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeepStormProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeepStormProject
+//
+//  Created by Alexandr Babenko on 07.08.16.
+//  Copyright © 2016 Alexandr Babenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
