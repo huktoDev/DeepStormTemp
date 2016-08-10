17883e48543939041d961714a9cb0408e7758e52
Alexandr Babenko <Hikto583004@list.ru>
2016-08-10 14:39:21 +0500

    update DSLocalSQLDatabase header

--- DeepStormProject/DSLocalSQLDatabase.h
+++ DeepStormProject/DSLocalSQLDatabase.h
@@ -7,31 +7,46 @@
 //
 
 #import <Foundation/Foundation.h>
-
-@class DSStreamingDatabaseEvent;
-@class DSAdaptedDBService, DSAdaptedDBJournal, DSAdaptedDBError, DSAdaptedDBJournalRecord;
-#import "DSAdaptedObjectsFactory.h"
+#import "DSStoreDataProvidingProtocol.h"
 
 @import CoreData;
 @class NSManagedObjectContext;
 
-@interface DSLocalSQLDatabase : NSObject
+@protocol DSStoreDataProvidingProtocol;
+@protocol DSStreamingEventExecutorProtocol;
+@class DSAdaptedDBService, DSAdaptedDBJournal, DSAdaptedDBError, DSAdaptedDBJournalRecord;
 
+#import "DSAdaptedObjectsFactory.h"
+
+@interface DSLocalSQLDatabase : NSObject <DSStreamingEventExecutorProtocol, DSStoreDataProvidingProtocol>
+
+
+#pragma mark - Construction
 + (instancetype)sharedDeepStormLocalDatabase;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 
-- (void)executeSendingEvent:(DSStreamingDatabaseEvent*)databaseEvent;
+#pragma mark - DSStreamingEventExecutorProtocol IMP
+// Локальная БД способна обрабатывать транзакции определенного типа
 
+- (BOOL)executeStreamingEvent:(id<DSStreamingEventProtocol>)databaseEvent;
+//TODO: make Pull Request
+
+
+#pragma mark - ENTITIES Factory
+// Фабрика "адаптированных" объектов (представляющих реальные объекты на контексте)
 
 @property (strong, nonatomic, readonly) DSAdaptedObjectsFactory *modelsFactory;
 
-- (NSArray<DSAdaptedDBJournal*>*)loadAllJournals;
-- (NSArray<DSAdaptedDBService*>*)loadAllServices;
-- (NSArray<DSAdaptedDBJournalRecord*>*)loadAllRecords;
 
-- (DSAdaptedDBJournal*)loadJournalForName:(NSString*)journalName;
-- (DSAdaptedDBJournal*)loadServiceForName:(NSString*)serviceName;
+#pragma mark - LOAD AND GET Objects
+// Получение объектов из Базы данных
+
+- (NSArray<DSBaseLoggedService*>*)getAllServices;
+- (NSArray<DSJournal*>*)getAllJournals;
+- (NSArray<DSJournalRecord*>*)getAllRecords;
+
+- (DSJournal*)getJournalForName:(NSString*)journalName;
+- (DSBaseLoggedService*)getServiceByTypeID:(NSNumber*)serviceTypeID orByClass:(NSString*)serviceClass;
 
 
 @end
